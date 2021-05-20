8fc80a0766067fde896a1c89181587f1a6417b8f
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-05-20 14:54:37 -0400

    added workout model

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercise: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+            name: {
+                type: String,
+                trim: true,
+            },
+            weight: {
+                type: Number,
+                default: 0,
+            },
+            sets: {
+                type: Number,
+                default: 0,
+            },
+            reps: {
+                type: Number,
+                default: 0,
+            },
+            duration: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ],
+    totDuration: {
+        type: Number,
+    }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
