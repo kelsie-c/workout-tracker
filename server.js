f9df173734da5831fb185bf355a49cec725390c2
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-05-27 14:58:21 -0400

    deploying to heroku

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNerUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 app.use(require("./routes/api.js"));
